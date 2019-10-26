f3b1454e6c884cb8cf982434ef002b508bbe535c
Wade <microsoftbi@163.com>
2019-10-27 00:07:49 +0800

    APP added

---
+++ APP/DWHGenerator/CodeGenerator/PSA.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeGenerator
+{
+    class PSA
+    {
+        public static string GenerateLandingZone()
+        { 
+            string result = "";
+
+            DataClassesDataContext dc = new DataClassesDataContext();
+
+            List<string> tables = (from p in dc.ATTRIBUTE select p.TABLE_NAME).ToList();
+
+
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var tableitem in tables.Distinct())
+            {
+                sb.AppendLine("CREATE TABLE [dbo].["+ tableitem+"]");
+                sb.AppendLine("(");
+
+
+
+                sb.AppendLine(")");
+            }
+
+            result = sb.ToString();
+
+            return result;
+        }
+    }
+}
