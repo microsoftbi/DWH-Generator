624e0cfaed06a4bb0e2aabfca44e37b02b6b8fa1
Wade <Microsoftbi@163.com>
2019-11-26 03:12:14 +0000

    Meta structure adjusted.

--- Databases/META/Tables/dbo.RecordSource.sql
+++ Databases/META/Tables/dbo.RecordSource.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[RecordSource]
 (
 [ID] [int] NOT NULL IDENTITY(1, 1),
 [RecordSourceName] [nvarchar] (20) COLLATE Chinese_PRC_CI_AS NULL,
+[DatabaseName] [nvarchar] (20) COLLATE Chinese_PRC_CI_AS NULL,
 [RecordSourceDesc] [nvarchar] (100) COLLATE Chinese_PRC_CI_AS NULL
 ) ON [PRIMARY]
 GO
