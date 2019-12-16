55c075a5002cdc4d9648525d1d3e3b660ba739bf
Wade <microsoftbi@163.com>
2019-12-16 15:42:32 +0800

    landing config added.

---
+++ Databases/META/Tables/dbo.DATAVAULT.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[DATAVAULT]
+(
+[ID] [int] NULL,
+[TYPE] [nvarchar] (1) COLLATE Chinese_PRC_CI_AS NULL,
+[TableName] [nvarchar] (30) COLLATE Chinese_PRC_CI_AS NULL
+) ON [PRIMARY]
+GO
